e020325ff82b74fd48d15090b0be4c5683042986
Filipe Crespo <filipe.crespo@locaweb.com.br>
2021-03-02 00:30:26 -0300

    new exercises

---
+++ exercism/ruby/luhn/luhn.rb
@@ -0,0 +1,21 @@
+=begin
+Write your code for the 'Luhn' exercise in this file. Make the tests in
+`luhn_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/luhn` directory.
+=end
+
+class Luhn
+  def self.valid?(value)
+    total = 0;
+    value.each do |x|
+      total = total + x
+    end
+    if total % 10 == 0
+      return true
+    else
+      return false
+    end
+  end
+end
